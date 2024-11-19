65cf3bf509c3a96d72535eef15109c187de402f9
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-19 16:48:15 +0100

    commit exercise palidromo

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -1,8 +1,10 @@
+//CHIEDERE ALL'UTENTE DI INSERIRE UNA PAROLA
 const parolaDaInserire = prompt('Inserisci una parola:');
 
+//FUNZIONE
 function isPalindromo(parola){
   let parolaInvertita = parola.split('').reverse().join('');
   return parolaInvertita === parolaDaInserire;
-
 }
-console.log(isPalindromo('radar'));
+//STAMPO LA MIA PAROLA PALINDROMA
+console.log(parolaDaInserire);
