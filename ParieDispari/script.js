d286bd5fb42acc85f9f446d4f2a7b23f02e01de6
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-19 18:05:04 +0100

    2 exercise completed without function

--- ParieDispari/script.js
+++ ParieDispari/script.js
@@ -2,13 +2,21 @@ const numGiocatore = Math.ceil(Math.random() * 5)
 const numComputer = Math.ceil(Math.random() * 5)
 
 let somma = numGiocatore + numComputer;
+let messaggio = '';
+let sceltaGiocatore = 'PARI';
 
 console.log('Il numero giocatore uscito è' ,numGiocatore );
 console.log('Il numero computer uscito è' ,numComputer);
 
-if (somma % 2){
+if (!(somma % 2)){
   messaggio = 'PARI'
 } else {
   messaggio = 'DISPARI'
 }
 console.log(messaggio);
+
+if(messaggio === sceltaGiocatore){
+  console.log('Il Giocatore ha vinto!');
+} else {
+  console.log('Il Computer ha vinto!');
+}
